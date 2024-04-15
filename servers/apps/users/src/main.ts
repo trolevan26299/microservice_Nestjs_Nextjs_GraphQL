46096b7dbcc0ae9d0b8c5a04828f5125342c58a3
Andrew <trolevan26299@gmail.com>
2024-04-15 17:00:13 +0700

    update code

---
+++ servers/apps/users/src/main.ts
@@ -0,0 +1,20 @@
+/* eslint-disable prettier/prettier */
+import { NestFactory } from "@nestjs/core";
+import { UsersModule } from "./users.module";
+import { NestExpressApplication } from "@nestjs/platform-express";
+import { join } from "path";
+
+async function bootstrap() {
+  const app = await NestFactory.create<NestExpressApplication>(UsersModule);
+
+  app.useStaticAssets(join(__dirname, "..", "public"));
+  app.setBaseViewsDir(join(__dirname, "..", "servers/email-templates"));
+  app.setViewEngine("ejs");
+
+  app.enableCors({
+    origin: "*",
+  });
+
+  await app.listen(4001);
+}
+bootstrap();
